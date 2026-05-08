68ef2acdbcca96f0066cabee3f7480c8da42a134
anwar789456 <anouarsalhi123@gmail.com>
2026-05-08 11:58:19 +0100

    deploying

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -e
 
-REPO_DIR="/var/www/logiciel-front"
-FRONT_DIR="$REPO_DIR/logiciel-samet-front"
+FRONT_DIR="/var/www/logiciel-front"
 
 echo "📦 Pulling latest changes from GitHub..."
-cd "$REPO_DIR"
+cd "$FRONT_DIR"
 git pull origin main
 
 echo "📦 Installing dependencies..."
-cd "$FRONT_DIR"
 npm install
 
 echo "🔨 Building frontend..."
